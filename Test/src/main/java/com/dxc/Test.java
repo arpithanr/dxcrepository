7f9f257bb67b9cd598282fb3074f5cf03e212a73
arpithanr <arpithanr98@gmail.com>
2020-09-26 17:53:14 +0530

    Welcome to DXC TECHNOLOGY

---
+++ Test/src/main/java/com/dxc/Test.java
@@ -0,0 +1,14 @@
+package com.dxc;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello, Welcome to DXC TECHNOLOGY");
+		
+		System.out.println("Hi!!!!! Friends");
+
+	}
+
+}
